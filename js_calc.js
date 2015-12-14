2e88b26198bc75f12f7145a8f465d705d901d09f
smurfik <smurfik@mlpinit.com>
2015-12-14 15:52:02 -0800

    add functionality

--- js_calc.js
+++ js_calc.js
@@ -1,27 +1,39 @@
 window.onload = function() {
 
-  var firstNum = document.getElementById("first").value;
-  var secondNum = document.getElementById("second").value;
+  var answer;
+  var ans = document.getElementById("answer");
 
   var submit = document.getElementById("submit");
 
   submit.addEventListener('click', function (event){
-    var firstNum = document.getElementById("first").value;
-    var n = parseInt(firstNum, 10);
-    var secondNum = document.getElementById("second").value;
-    var m = parseInt(secondNum, 10);
+    var firstNum = document.getElementById("first");
+    var n = parseInt(firstNum.value, 10);
+    var secondNum = document.getElementById("second");
+    var m = parseInt(secondNum.value, 10);
     var operation = document.getElementById("sign");
     var num = operation.options[operation.selectedIndex].value;
 
     event.preventDefault();
     if (num == "1") {
-      console.log(n + m);
+      answer = n + m;
+      ans.value = answer;
+      firstNum.value = answer;
+      secondNum.value = "";
     } else if (num == "2") {
-      console.log(n - m);
+      answer = n - m;
+      ans.value = answer;
+      firstNum.value = answer;
+      secondNum.value = "";
     } else if (num == "3") {
-      console.log(n * m);
+      answer = n * m;
+      ans.value = answer;
+      firstNum.value = answer;
+      secondNum.value = "";
     } else {
-      console.log(n/4);
+      answer = n/m;
+      ans.value = answer;
+      firstNum.value = answer;
+      secondNum.value = "";
     };
   });
 };
