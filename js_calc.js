7e1d27e71b03fc0d4a567d1ca8025a2cc4a507d5
smurfik <smurfik@mlpinit.com>
2015-12-15 13:58:07 -0800

    remove if stmt outside of html js

--- js_calc.js
+++ js_calc.js
@@ -1,30 +1,35 @@
+var answer;
+var calc = function (n, num, m) {
+  if (num == "1") {
+    answer = n + m;
+  } else if (num == "2") {
+    answer = n - m;
+  } else if (num == "3") {
+    answer = n * m;
+  } else {
+    answer = n/m;
+  }
+  return answer
+};
+
 window.onload = function() {
 
   var answer;
   var ans = document.getElementById("answer");
-
   var submit = document.getElementById("submit");
   var firstNum = document.getElementById("first");
+  var secondNum = document.getElementById("second");
+  var operation = document.getElementById("sign");
+  var n, m, num;
 
   submit.addEventListener('click', function (event) {
-    var n = parseInt(firstNum.value, 10);
-    var secondNum = document.getElementById("second");
-    var m = parseInt(secondNum.value, 10);
-    var operation = document.getElementById("sign");
-    var num = operation.options[operation.selectedIndex].value;
+    n = parseInt(firstNum.value, 10);
+    m = parseInt(secondNum.value, 10);
+    num = operation.options[operation.selectedIndex].value;
 
     event.preventDefault();
-    if (num == "1") {
-      answer = n + m;
-    } else if (num == "2") {
-      answer = n - m;
-    } else if (num == "3") {
-      answer = n * m;
-    } else {
-      answer = n/m;
-    };
-    ans.value = answer;
-    firstNum.value = answer;
+    ans.value = calc(n, num, m);
+    firstNum.value = calc(n, num, m);
   });
 
   var clear = document.getElementById("clear");
